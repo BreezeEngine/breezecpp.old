e27259804e36b098bbe162f3742915a0e0e30abd
Maik Klein <maikklein@googlemail.com>
2016-01-15 15:08:00 +0100

    cleanup ecs example

--- examples/ecs/main.cpp
+++ examples/ecs/main.cpp
@@ -12,7 +12,7 @@ struct name {
   std::string name;
 };
 
-struct print_system {
+struct print_name {
   template <class World> void update(World& w) {
     w.template update<name>([](auto& name) { print(name.name); });
   }
@@ -34,14 +34,11 @@ using component_group_handle = ecs::core::component_group<
     breeze::util::container::handle_container, Ts...>;
 
 int main() {
-
-  using namespace hana::literals;
-
   using moveable_object = component_group_handle<position, velocity>;
   using entity = component_group_handle<position, velocity, name>;
   using cg = ecs::core::component_groups<moveable_object, entity>;
   auto w = ecs::core::world<cg>{};
-  auto sg = ecs::core::make_systems_group(w, print_system{}, print_pos_vel{});
+  auto sg = ecs::core::make_systems_group(w, print_name{}, print_pos_vel{});
   w.add(make_monster(10, 5, "Monster3"));
   w.add(make_monster(11, 5, "Monster2"));
   w.add(position{ 42, 24 }, velocity{ 5, 5 }, name{ "Monster1" });
